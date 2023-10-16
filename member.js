bb592374cd1c684678bf6b10e6b75698bf39e418
Aleksandar <alexandar.gordic@gmail.com>
2023-10-16 20:20:09 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -2,7 +2,6 @@ function skillMember() {
   return {
     name: 'skillMember',
     path: '/skill/member',
-    component: () => import('@/views/skill/member'),
     meta: {
       title: '技能成员',
       icon: 'skill',
